ccf49727ca4bbe07da2b8bab0a47afa103608e33
Thomas <thomaspinton@alunos.utfpr.edu.br>
2024-06-10 20:28:32 -0300

    mysql connector adition with tests

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 from tkinter import *
+from tkinter import ttk
+import mysql.connector
 
-def main():
-    print("hello world")
+def connectToSQL():
+    import mysql.connector
 
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="honeyhub"
+    )
 
+    print('teste')
 
-if __name__ == "__main__":
-    main()
+    cursor = mydb.cursor()
+
+    cursor.execute("show tables")
+
+    respose = cursor.fetchall()
+    
+    print(respose)
+
+    cursor.execute("select * from community")
+
+    respose = cursor.fetchall()
+
+    print(respose)
+    
+    print(mydb)
+
+connectToSQL()
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+
+frm.grid()
+
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+root.mainloop()
