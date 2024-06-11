d9711d26aaa84a40f0a4484322551335b7e0a723
vkzy <vkzya@LAPTOP-V2LRBOMP>
2024-06-11 18:01:20 -0300

    selecting schema

--- main.py
+++ main.py
@@ -7,46 +7,50 @@ def main():
 
 mydb = 0
 cursor = 0
+user_global = ""
+password_global = ""
 
 def connectToSQL(user, password):
-
     global mydb
     global cursor
 
     try:
         mydb = mysql.connector.connect(
             host="localhost",
-            user= user,
-            password = password,
-            database="university"
+            user=user,
+            password=password
         )
 
         cursor = mydb.cursor()
+        cursor.execute("show databases")
+        response = cursor.fetchall()
 
-        
+        return True, response
 
-        cursor.execute("show tables")
+    except mysql.connector.Error as err:
+        print("error")
+        return False, []
 
-        respose = cursor.fetchall()
-        
-        # print(respose)
+def selectSchema(schema):
+    global mydb
+    global cursor
+    global user_global
+    global password_global
 
-        cursor.execute("select * from department")
-
-        respose = cursor.fetchall()
-
-        # print(respose)
-        
-        # print(mydb)
-
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=user_global,
+            password=password_global,
+            database=schema
+        )
+        cursor = mydb.cursor()
         return True
-
     except mysql.connector.Error as err:
         print("error")
         return False
 
 def getColumnInformation(column):
-
     global mydb
     global cursor
 
@@ -91,7 +95,7 @@ def createLoginWindow():
     ttk.Label(frm, text="Select database: ").grid(column=0, row=2, sticky=E, pady=5)
     db_type_combo = ttk.Combobox(frm, textvariable=db_type_var, values=["MySQL", "PostgreSQL"])
     db_type_combo.grid(column=1, row=2, sticky=(W, E), pady=5)
-    db_type_combo.current(0) 
+    db_type_combo.current(0)
 
     login_button = ttk.Button(frm, text="Login", command=lambda: login(user_input, pass_input, error_lbl, login_window))
     login_button.grid(column=1, row=3, sticky=(W), pady=5)
@@ -102,23 +106,52 @@ def createLoginWindow():
     login_window.mainloop()
 
 def login(user_input, pass_input, error_lbl, login_window):
+    global user_global, password_global
     user = user_input.get()
     password = pass_input.get()
-    success = connectToSQL(user, password)
+    user_global = user
+    password_global = password
+    success, schemas = connectToSQL(user, password)
     if(success):
         print("successful login")
         error_lbl.config(text="")
         login_window.destroy()
-        openDbWindow()
-        getColumnInformation('student')
-
+        selectSchemaWindow(schemas)
     else:
         print("error")
         error_lbl.config(text="user and/or password incorrect")
 
-def openDbWindow():
+def selectSchemaWindow(schemas):
+    schema_window = Tk()
+    schema_window.title("Select Schema:")
+    schema_window.geometry('400x300')
+
+    frm = ttk.Frame(schema_window, padding=20)
+    frm.grid()
+
+    ttk.Label(frm, text="Select schema:").grid(column=0, row=0, columnspan=2, pady=5)
+    db_var = StringVar()
+    db_combo = ttk.Combobox(frm, textvariable=db_var, values=[db[0] for db in schemas])
+    db_combo.grid(column=1, row=0, pady=5)
+    db_combo.current(0)
+
+    schema_button = ttk.Button(frm, text="Select", command=lambda: schemaSelected(db_var.get(), schema_window))
+    schema_button.grid(column=1, row=1, sticky=(W), pady=5)
+
+    schema_window.mainloop()
+
+def schemaSelected(schema, db_window):
+    success = selectSchema(schema)
+    if(success):
+        print("schema selected")
+        db_window.destroy()
+        viewDbWindow()
+    else:
+        print("error")
+
+def viewDbWindow():
     db_window = Tk()
-    db_window.title("select database:")
+    db_window.title("Database Operations:")
     db_window.geometry('400x300')
 
     frm = ttk.Frame(db_window, padding=20)
